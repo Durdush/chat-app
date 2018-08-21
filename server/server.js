bd2fc1b3fa471f80ad39718943a772b7b267727c
Neelesh Malpani <malpanineelesh@gmail.com>
2018-08-21 12:25:11 +0530

    changed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname,'../public');
+// const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname,'../public')));
 app.listen(port,()=>{
       console.log(publicPath);
       console.log(`server is up on ${port}`);    
