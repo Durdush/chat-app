627f76860128e30d216f37dc3094f1d0baa5d2bc
Neelesh Malpani <malpanineelesh@gmail.com>
2018-08-22 00:28:56 +0530

    change

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
-// const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection',(socket)=>{
+   console.log('new user connected');
+})
 app.use(express.static(path.join(__dirname,'../public')));
-app.listen(port,()=>{
-      console.log(publicPath);
+server.listen(port,()=>{
+      // console.log(publicPath);
       console.log(`server is up on ${port}`);    
 })
